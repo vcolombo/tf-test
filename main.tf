515b5a2345ff12d7b8023cfc47d899d5a0cd6fbf
Vincent Colombo <vcolombo@medstrat.com>
2023-12-02 14:55:56 -0600

    Commit 3

--- main.tf
+++ main.tf
@@ -38,15 +38,15 @@ provider "aws" {
 
 # Add .gitignore file in this directory with the terraform.tfvars
 
-resource "aws_instance" "tc_instance" {
-  ami           = "ami-0c7c4e3c6b4941f0f"
-  instance_type = "t2.micro"
+# resource "aws_instance" "tc_instance" {
+#   ami           = "ami-0c7c4e3c6b4941f0f"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "TC-triggered-instance"
-  }
-}
+#   tags = {
+#     Name = "TC-triggered-instance"
+#   }
+# }
 
-resource "aws_iam_user" "test_user_99" {
-  name = "test-user-99"
-}
+# resource "aws_iam_user" "test_user_99" {
+#   name = "test-user-99"
+# }
